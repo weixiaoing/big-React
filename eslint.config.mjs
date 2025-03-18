8c0a8fffb567c8da6c3a6c906e94bdc51b37592a
dawnot <1537476031@qq.com>
2025-03-18 14:20:43 +0800

    feat: jsx函数编写,打包,eslint取消any判定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,56 @@
-import { defineConfig } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
 });
 
-export default defineConfig([{
-    extends: compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "plugin:prettier/recommended",
-    ),
+export default defineConfig([
+	{
+		extends: compat.extends(
+			'eslint:recommended',
+			'plugin:@typescript-eslint/recommended',
+			'prettier',
+			'plugin:prettier/recommended'
+		),
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        prettier,
-    },
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+			prettier
+		},
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            ...globals.jest,
-        },
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.jest
+			},
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
+			parser: tsParser,
+			ecmaVersion: 'latest',
+			sourceType: 'module'
+		},
 
-    rules: {
-        "prettier/prettier": "error",
-        "no-case-declarations": "off",
-        "no-constant-condition": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-var-requires": "off",
-        "no-unused-vars": "off",
-    },
-}]);
+		rules: {
+			'prettier/prettier': 'error',
+			'no-case-declarations': 'off',
+			'no-constant-condition': 'off',
+			'@typescript-eslint/ban-ts-comment': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-var-requires': 'off',
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-explicit-any': 'off'
+		}
+	}
+]);
---
+++ packages/react/index.ts
@@ -0,0 +1,6 @@
+import { jsx } from './src/jsx';
+// react包 将jsx作为createElement导出
+export default {
+	version: '1.0.0',
+	createElement: jsx
+};
